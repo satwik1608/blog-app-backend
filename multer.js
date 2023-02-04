995bad27b03bd51d05a2dafa370b74e875816eb5
satwik1608 <satwikkashyap1608@gmail.com>
2023-02-04 19:55:22 +0530

    ok

--- multer.js
+++ multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const path = require("path");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "/uploads/"));
+    cb(null, __dirname);
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
