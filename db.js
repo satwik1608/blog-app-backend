25ce4d63f73d3c2f13c76b37cc1fe96c8acba0b4
satwik1608 <satwikkashyap1608@gmail.com>
2023-02-07 02:02:25 +0530

    f

--- db.js
+++ db.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const dbURI =
   "mongodb+srv://satwik12:satwik12@cluster0.byld6up.mongodb.net/test";
 
+// const dbURI = "mongodb://localhost/blogApp";
+
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
