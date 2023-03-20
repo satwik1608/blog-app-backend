37dc551bc43b6b0010899e6616ada3cb10dd36c0
satwik1608 <satwikkashyap1608@gmail.com>
2023-03-20 12:04:55 +0530

    multer fully removed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const api = require("./api");
 const db = require("./db");
-const mul = require("./multer");
+
 const app = express();
 
 const middleware = require("./middleware");
